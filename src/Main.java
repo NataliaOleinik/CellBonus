cbbe6864d92621b2f324ac98a01ab7bb95ff9bc7
Natalia Oleinik <olnataly81@gmail.com>
2020-09-23 10:41:14 +0700

    main edited

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
         int accountStatus = 10000;
-        int accountRefill = 100000;
-        int bonus = accountRefill/100;
+        int accountRefill = 10000;
         if(accountRefill >=100000){
+            int bonus = accountRefill/100;
             accountStatus += accountRefill + bonus;
         }else{
            accountStatus += accountRefill;
